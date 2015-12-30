7e03adea80c52621e62c6bbb8edde1af54c18203
Luiz Gustavo Freire Gama <luizgama@onrails.com.br>
2015-12-30 09:26:28 -0200

    motd

--- vendor/cookbooks/custombox/recipes/default.rb
+++ vendor/cookbooks/custombox/recipes/default.rb
@@ -26,6 +26,10 @@ template '/home/vagrant/.bashrc_template' do
 	# group 'vagrant'
 end
 
+template '/etc/motd' do
+	source 'motd_template'
+end
+
 # execute 'install rails on default rvm ruby' do
 # 	command "rvm use default"
 # 	command "gem install rails"
@@ -35,6 +39,10 @@ end
 
 execute 'append_to_bashrc' do
 	command "cat /home/vagrant/.bashrc_template >> /home/vagrant/.bashrc"
+end
+
+execute 'install_dependencies'
+	command "sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libgmp3-dev"
 end
 
 file '/home/vagrant/.bashrc_template' do
