8f07ee8c1cedd56aef45a2968c48c81f7c026a2c
Luiz Gustavo Freire Gama <luizgama@onrails.com.br>
2017-05-28 10:58:10 -0300

    metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'RailsBox2016'
+name             'RailsBox'
 maintainer       'Luiz Gustavo Freire Gama'
 maintainer_email 'luizgama@onrails.com.br'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs/Configures RailsBox2016'
 long_description 'Installs/Configures RailsBox2016'
 
